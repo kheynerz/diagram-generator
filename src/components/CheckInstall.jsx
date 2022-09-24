94004f41c3a52b0b96d7e7ad2c8d7a43b3a0c15f
kheynerz <sebastiancr44@gmail.com>
2022-09-23 23:31:04 -0600

    Merge Main

--- src/components/CheckInstall.jsx
+++ src/components/CheckInstall.jsx
@@ -1,14 +1,15 @@
 import CheckInstallModal from './Modals/CheckInstallModal';
 import { useCheckInstall } from '../hooks/useCheckInstall';
 
-
+import { Snackbar } from './Snackbar';
 
 const CheckInstall = () => {
     const {installed, fetching, connected} = useCheckInstall()
 
     if (fetching) return <p>Loading...</p>
-    if (installed) return <h1>Instalado</h1>
+    if (installed) return <Snackbar body='Los procedimientos ya se encuentran instalados' header='Procedimientos Instalados'/>
     if (connected) return <CheckInstallModal  />
+    
 
     return  <h1>bad credentials</h1>
 }
